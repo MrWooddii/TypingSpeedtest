c538b8aedf319704d8a5c054be979d4dee86a5af
Stephan Holzapfel <stephenholzapfel@aol.com>
2021-07-19 17:51:18 +0200

    playAgain wpm, keystrokes timer

--- src/main/Controllers/gameController.java
+++ src/main/Controllers/gameController.java
@@ -1,20 +1,35 @@
 package main.Controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-
+import javafx.scene.input.MouseEvent;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class gameController implements Initializable {
 
-    private List<String> wordList = new ArrayList<>();
+    private final int TIME_RESTRICTION = 60;
+
+    private final List<String> wordList = new ArrayList<>();
+
+    private boolean gameStarted = false;
+
+    private int correctKeystrokes = 0;
+
+    private int wrongKeystrokes = 0;
+
+    private double accuracy = 100;
+
+    private int time;
+
+    private Timer timer;
 
     @FXML
     private Label firstWord;
@@ -26,9 +41,18 @@ public class gameController implements Initializable {
     @FXML
     private Label seconds;
     @FXML
-    private Label keystrokes;
+    private Label correctKeystrokesLabel;
     @FXML
-    private Label accuracy;
+    private Label wrongKeystrokesLabel;
+    @FXML
+    private Label accuracyLabel;
+    @FXML
+    private Label wpmLabel;
+
+    @FXML
+    private Button playAgainButton;
+    @FXML
+    private Button highscoreButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -46,44 +70,112 @@ public class gameController implements Initializable {
             System.out.println("FileNotFoundException: " + e.getMessage());
         }
 
-        Collections.shuffle(wordList);
-
-        int index = 0;
-
-        firstWord.setText(wordList.get(index));
-        if(index + 1 < wordList.size()) {
-            secondWord.setText(wordList.get(index + 1));
-        } else {
-            secondWord.setText("");
-        }
+        setWords();
+        time = TIME_RESTRICTION;
+        seconds.setText("" + time);
+        accuracyLabel.setText("" + accuracy);
+        wrongKeystrokesLabel.setText("" + wrongKeystrokes);
+        correctKeystrokesLabel.setText("" + correctKeystrokes);
 
         enteredWord.requestFocus();
-
-        //TODO: löschen, nur zum Test
-        for(String word : wordList) {
-            System.out.println(word);
-        }
     }
 
     //startet, sobald ein Buchstabe in "enteredWord" eingegeben wird
-    //TODO: play button abändern, Stand jetzt startet er auch als erstes das Game (wirft Exception)
     @FXML
     public void startGame(KeyEvent keyEvent) throws InterruptedException{
 
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        //executor.scheduleAtFixedRate();
+        if(gameStarted) {
 
-        /*
-
-        while (Integer.parseInt(seconds.getText()) > 0) {
-            firstWord.setText(wordList.get(index));
-            if(index + 1 < wordList.size()) {
-                secondWord.setText(wordList.get(index + 1));
-            } else {
-                secondWord.setText("");
+            if(keyEvent.getCode().equals(KeyCode.BACK_SPACE)) {
+                return;
             }
 
-        }*/
+            if(keyEvent.getCode().equals(KeyCode.SPACE)) {
+                checkWord(enteredWord.getText().trim(), firstWord.getText());
+
+                //if no wrong keystrokes are registered, accuracy is 100 %
+                accuracy = wrongKeystrokes == 0 ? 100 : (double) correctKeystrokes / (wrongKeystrokes + correctKeystrokes) * 100;
+
+                accuracyLabel.setText("" + (int) accuracy);
+
+                firstWord.setText(secondWord.getText());
+                secondWord.setText(wordList.get((int) (Math.random() * wordList.size())));
+                enteredWord.clear();
+            }
+
+            return;
+        }
+
+        gameStarted = true;
+
+        timer =  new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if(time >= 0) {
+                    time--;
+
+                    Platform.runLater(new Runnable() {
+                       @Override
+                       public void run() {
+                           seconds.setText("" + time);
+                       }
+                    });
+                }
+
+                if(time <= 0) {
+                    System.out.println("Time is up!");
+                    enteredWord.clear();
+                    enteredWord.setDisable(true);
+                    highscoreButton.setVisible(true);
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
     }
+
+    private void checkWord(String enteredWord, String correctWord) {
+
+        //Shift and space count as one additional keystroke each
+        int length = Character.isUpperCase(enteredWord.charAt(0)) ? enteredWord.length() + 2 : enteredWord.length() + 1;
+
+        if(enteredWord.equals(correctWord)) {
+            correctKeystrokes += length;
+            correctKeystrokesLabel.setText("" + correctKeystrokes);
+            //one word per minute is defined as 5 keystrokes == one word
+            wpmLabel.setText("" + (correctKeystrokes / 5));
+        } else {
+            wrongKeystrokes += length;
+            wrongKeystrokesLabel.setText("" + wrongKeystrokes);
+        }
+    }
+
+    @FXML
+    private void playAgain(MouseEvent event) {
+
+        time = TIME_RESTRICTION;
+        seconds.setText("" + time);
+        correctKeystrokes = 0;
+        correctKeystrokesLabel.setText("" + correctKeystrokes);
+        wrongKeystrokes = 0;
+        wrongKeystrokesLabel.setText("" + wrongKeystrokes);
+        accuracyLabel.setText("" + 100);
+        wpmLabel.setText("" + 0);
+        enteredWord.setDisable(false);
+        highscoreButton.setVisible(false);
+        setWords();
+        timer.cancel();
+        gameStarted = false;
+        enteredWord.requestFocus();
+
+    }
+
+    private void setWords() {
+        firstWord.setText(wordList.get((int) (Math.random() * wordList.size())));
+        secondWord.setText(wordList.get((int) (Math.random() * wordList.size())));
+    }
+
 
 }
